b884fed50e9d28a54e619a0b962ffc7eb41a0995
moktadir-mirul <moktadir.mirul@gmail.com>
2025-02-26 17:16:46 +0600

    add money js done

--- scripts/addMoney.js
+++ scripts/addMoney.js
@@ -1,4 +1,17 @@
 document.getElementById('add-btn').addEventListener('click', function(event) {
     event.preventDefault();
-    console.log("hello")
+    const addAmount = document.getElementById('add-amount').value;
+    const addAmountNumber = parseFloat(addAmount);
+    const addPin = document.getElementById('add-pin').value;
+    const pinNumber = parseInt(addPin);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedBalance = parseFloat(mainBalance);
+
+    if(pinNumber === 1234) {
+        const sum = addAmountNumber + convertedBalance;
+        document.getElementById("main-balance").innerText = sum;
+        document.getElementById('add-amount').value='';
+    } else {
+        alert("wrong pin");
+    }
 })
