ad9ebe8efb4a084d12132e49c5843b39d79ad1c2
moktadir-mirul <moktadir.mirul@gmail.com>
2025-02-26 16:24:52 +0600

    login page done

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,15 @@
 document.getElementById('login').addEventListener('click', function(){
-    console.log("hello");
-    
+    const accNumber = document.getElementById("acc-num").value;
+    const accPin = document.getElementById('pin').value;
+    const pinNumber = parseInt(accPin);
+    console.log(pinNumber, "pin", accNumber, "accNumber", accNumber.length);
+    if(accNumber.length === 11) {
+        if(pinNumber === 1234) {
+            window.location.href = "./pages.html";
+        } else {
+            alert("wrong pin number");
+        }
+    } else{
+        alert('Wrong account number');
+    }
 })
