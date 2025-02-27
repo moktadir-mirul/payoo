4bce800a6cd2f4266961cb9203d74d6292edd7d5
moktadir-mirul <moktadir.mirul@gmail.com>
2025-02-27 15:41:06 +0600

    minor change in logic of cashout

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -1,5 +1,6 @@
 document.getElementById("out-btn").addEventListener("click", function(event){
     event.preventDefault();
+    const agentAcc = document.getElementById("agent-acc").value;
     const outAmount = document.getElementById("out-amount").value;
     const amountInNumber = parseFloat(outAmount);
     const pin = document.getElementById("out-pin").value;
@@ -7,11 +8,15 @@ document.getElementById("out-btn").addEventListener("click", function(event){
     const mainBalance = document.getElementById("main-balance").innerText;
     const balanceNumber = parseFloat(mainBalance);
     
-    if(pinNumber === 1234) {
-        const diff = balanceNumber - amountInNumber;
-        document.getElementById("main-balance").innerText = diff;
-        document.getElementById("out-amount").value = "";
+    if (agentAcc.length === 11 && !isNaN(amountInNumber)) {
+        if(pinNumber === 1234) {
+            const diff = balanceNumber - amountInNumber;
+            document.getElementById("main-balance").innerText = diff;
+            document.getElementById("out-amount").value = "";
+        } else {
+            alert('Incorrect Pin')
+        }
     } else {
-        alert('Incorrect Pin')
+        alert("invalid agent number or invalid amount")
     }
 })
